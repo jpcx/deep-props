804ea9e4bbf6e9e62a0df3ed909aff392c6ba688
Justin Collier <jpcxme@gmail.com>
2018-05-02 19:27:00 -0700

    fixed tilde URL encoding

--- build/markdown.js
+++ build/markdown.js
@@ -290,13 +290,13 @@ const labelNameTags = string => string.split(
 
       if (namespaceMatch !== undefined) {
         opData.looking = false
-        line = '<a name=~' + namespaceMatch + '></a>' + line
+        line = '<a name=%7E' + namespaceMatch + '></a>' + line
       } else if (staticMatch !== undefined) {
         opData.looking = false
-        line = '<a name=~' + staticMatch + '></a>' + line
+        line = '<a name=%7E' + staticMatch + '></a>' + line
       } else if (typeMatch !== undefined) {
         opData.looking = false
-        line = '<a name=~' + typeMatch + '></a>' + line
+        line = '<a name=%7E' + typeMatch + '></a>' + line
       }
 
       opData.newString = line + opData.newString
